0af213cc135ea3314ed44ce39ad970884eb77b90
AbdullahMufti <abdullahmufty@gmail.com>
2023-06-13 02:28:43 +0500

    local build successful

--- components/icons.tsx
+++ components/icons.tsx
@@ -27,10 +27,21 @@ export const Icons = {
   twitter: Twitter,
   ShoppingCart: ShoppingCart,
   logo: (props: LucideProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-smartphone">
-  <rect width="14" height="20" x="5" y="2" rx="2" ry="2"/>
-  <path d="M12 18h.01"/>
-</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-smartphone"
+    >
+      <rect width="14" height="20" x="5" y="2" rx="2" ry="2" />
+      <path d="M12 18h.01" />
+    </svg>
   ),
   gitHub: (props: LucideProps) => (
     <svg viewBox="0 0 438.549 438.549" {...props}>
--- db/schema.ts
+++
@@ -1,5 +0,0 @@
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  fullName: text("full_name"),
-  phone: varchar("phone", { length: 256 }),
-})
