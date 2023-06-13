1a437e51f8c5841e5e6d03b06a0ccc94f436833b
AbdullahMufti <abdullahmufty@gmail.com>
2023-06-13 10:33:31 +0500

    hard coded envs

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,13 +1,14 @@
 import { createClient } from "next-sanity"
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const token = process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+const projectId = "jfk0nyjc"
+const dataset = "production"
+const token =
+  "skXNj7WAFqO0CHGNyPZpkbVLIogSe3FjhJ9UP6mIsZR0OwE70GJhAFLVIgWdNF56P5rNSUZywUj7ExE8cy3x9SQwhuG6db2PWYqzxGGu3omivmQegkTp7pxps7O3Xnpen4gr6BU2jOma0HL5FlIjJHt9GSsJt4vsA7KoZE5ZOVnszEDNMWOo"
+const apiVersion = "2022-11-15"
 export const client = createClient({
   projectId: projectId,
   dataset: dataset,
   useCdn: true,
-  apiVersion: apiVersion as string,
+  apiVersion: "2022-11-15",
   token: token,
 })
