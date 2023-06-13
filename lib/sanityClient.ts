a256e3f5ff5a1ad49171156921b14ae952b05366
AbdullahMufti <abdullahmufty@gmail.com>
2023-06-13 10:25:40 +0500

    api version from .env

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,9 +1,9 @@
-import { apiVersion } from "@/sanity/lib/env"
 import { createClient } from "next-sanity"
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 const token = process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 export const client = createClient({
   projectId: projectId,
   dataset: dataset,
